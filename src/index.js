187f414af3bceeda405919085e1f86b303ed755d
Vlad Solovey <connectwithvlad@gmail.com>
2023-02-13 14:01:32 +0300

    config: add eslint

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ function component() {
   const element = document.createElement('div');
 
   // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = "Hi, webpack";
+  element.innerHTML = 'Hi, webpack';
   element.classList.add('hello');
 
   return element;
