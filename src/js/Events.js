5a1932b7ae05362fe30a85d51601d6abf67ddde3
Vlad Solovey <connectwithvlad@gmail.com>
2023-03-03 00:34:00 +0300

    feat: add Events class to handle events

---
+++ src/js/Events.js
@@ -0,0 +1,28 @@
+class Events {
+  constructor() {
+    this.events = {};
+  }
+
+  on(eventName, handler) {
+    this.events[eventName] = this.events[eventName] ?? [];
+    this.events[eventName].push(handler);
+  }
+
+  off(eventName, handler) {
+    if (!this.events[eventName]) return;
+
+    const eventIndex = this.events[eventName].findIndex((fn) => fn === handler);
+    if (eventIndex > 0) {
+      this.events[eventName].splice(eventIndex, 1);
+    }
+  }
+
+  emit(eventName, data) {
+    if (!this.events[eventName]) return;
+    this.events[eventName].forEach((fn) => {
+      fn(data);
+    });
+  }
+}
+
+export default Events;
