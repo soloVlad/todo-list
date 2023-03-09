1ae494528d82dc53215c7e441c266d984239f5ef
Vlad Solovey <connectwithvlad@gmail.com>
2023-03-09 14:02:36 +0300

    refactor: change Events class to object

--- src/js/Events.js
+++ src/js/Events.js
@@ -1,12 +1,10 @@
-class Events {
-  constructor() {
-    this.events = {};
-  }
+const Events = {
+  events: {},
 
   on(eventName, handler) {
     this.events[eventName] = this.events[eventName] ?? [];
     this.events[eventName].push(handler);
-  }
+  },
 
   off(eventName, handler) {
     if (!this.events[eventName]) return;
@@ -15,14 +13,14 @@ class Events {
     if (eventIndex > 0) {
       this.events[eventName].splice(eventIndex, 1);
     }
-  }
+  },
 
   emit(eventName, data) {
     if (!this.events[eventName]) return;
     this.events[eventName].forEach((fn) => {
       fn(data);
     });
-  }
-}
+  },
+};
 
 export default Events;
