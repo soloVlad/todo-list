e7d170fcee6a9f4db62f4bb51012a5995794f0bf
Vlad Solovey <connectwithvlad@gmail.com>
2023-03-09 15:43:38 +0300

    refactor: change structre of FullList

--- src/js/FullList.js
+++ src/js/FullList.js
@@ -1,15 +1,26 @@
-class TaskList {
+class FullList {
   constructor(list = []) {
     this.list = list;
   }
 
   add(task) {
-    this.list.push(task);
+    const { listName } = task;
+    this.list[listName] = this.list[listName] ?? [];
+    this.list[listName].push(task);
   }
 
   remove(id) {
-    this.list = this.list.filter((task) => task.id !== id);
+    const listName = id.split('-')[0];
+    this.list[listName] = this.list[listName].filter((task) => task.id !== id);
+  }
+
+  getByListName(listName) {
+    return this.list[listName];
+  }
+
+  getAvailableId(listName) {
+    return this.list[listName][this.list[listName].length - 1] + 1;
   }
 }
 
-export default TaskList;
+export default FullList;
