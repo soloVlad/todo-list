0afc75ddd607d7ff17d03e7b957f13578bc125f6
Vlad Solovey <connectwithvlad@gmail.com>
2023-03-09 16:19:04 +0300

    feat: add ListModel

--- src/js/FullList.js
+++ src/js/FullList.js
@@ -1,26 +1,24 @@
-class FullList {
-  constructor(list = []) {
-    this.list = list;
-  }
+const FullList = {
+  list: [],
 
   add(task) {
     const { listName } = task;
     this.list[listName] = this.list[listName] ?? [];
     this.list[listName].push(task);
-  }
+  },
 
   remove(id) {
     const listName = id.split('-')[0];
     this.list[listName] = this.list[listName].filter((task) => task.id !== id);
-  }
+  },
 
   getByListName(listName) {
     return this.list[listName];
-  }
+  },
 
   getAvailableId(listName) {
     return this.list[listName][this.list[listName].length - 1] + 1;
-  }
-}
+  },
+};
 
 export default FullList;
---
+++ src/js/model/ListModel.js
@@ -0,0 +1,25 @@
+import FullList from '../FullList';
+
+class ListModel {
+  constructor(listName) {
+    this.listName = listName;
+    this.list = FullList.getByListName(this.listName);
+  }
+
+  add(task) {
+    const taskId = `${this.listName}-${FullList.getAvailableId(this.listName)}`;
+    const taskWithId = {
+      taskId,
+      ...task,
+    };
+    this.list.push(taskWithId);
+    FullList.add(taskWithId);
+  }
+
+  remove(id) {
+    this.listName = this.listName.filter((task) => task.id !== id);
+    FullList.remove(id);
+  }
+}
+
+export default ListModel;
