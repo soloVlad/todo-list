6d386d1978b196115034d0d02b011544e840f47a
Vlad Solovey <connectwithvlad@gmail.com>
2023-03-13 23:54:28 +0300

    feat: add basic footer

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+import Footer from './js/view/FooterView';
+
 import ListModel from './js/model/ListModel';
 import ListController from './js/controller/ListController';
 import ListView from './js/view/ListView';
@@ -5,3 +7,5 @@ import ListView from './js/view/ListView';
 const listModel = new ListModel('first');
 const listController = new ListController(listModel);
 const listView = new ListView(listController, document.body);
+
+const footerView = new Footer(document.body);
---
+++ src/js/view/FooterView.js
@@ -0,0 +1,26 @@
+import gitHubIcon from '../../img/github.svg';
+
+class Footer {
+  constructor(parent) {
+    this.footer = document.createElement('footer');
+    this.footerLink = document.createElement('a');
+    this.footerIcon = document.createElement('img');
+    this.footerText = document.createElement('span');
+
+    this.footer.classList.add('footer');
+    this.footerLink.classList.add('footer__link');
+    this.footerIcon.classList.add('footer__icon');
+    this.footerText.classList.add('footer__text');
+
+    this.footerLink.href = 'https://github.com/soloVlad/todo-list';
+    this.footerText.textContent = '@soloVlad';
+    this.footerIcon.src = gitHubIcon;
+
+    this.footer.appendChild(this.footerLink);
+    this.footerLink.appendChild(this.footerText);
+    this.footerLink.appendChild(this.footerIcon);
+    parent.appendChild(this.footer);
+  }
+}
+
+export default Footer;
